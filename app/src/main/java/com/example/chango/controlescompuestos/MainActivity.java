cd3cf54faec261b914275c17664f5364e4b19085
alumnoIdat <leomanarael@gmail.com>
2016-11-23 11:28:09 -0500

    final

--- app/src/main/java/com/example/chango/controlescompuestos/MainActivity.java
+++ app/src/main/java/com/example/chango/controlescompuestos/MainActivity.java
@@ -5,9 +5,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ControlLogin ctlControlLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ctlControlLogin = (ControlLogin)findViewById(R.id.CtlLogin);
+        ctlControlLogin.setOnLoginListener(new OnLoginListener(){
+            @Override
+            public void onLogin(String usuario, String password){
+                if (usuario.equals("demo") && password.equals("demo"))
+                    ctlControlLogin.setMensaje("Login correcto!");
+                else
+                    ctlControlLogin.setMensaje("Vuelva a intentarlo.");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/chango/controlescompuestos/OnLoginListener.java
@@ -0,0 +1,9 @@
+package com.example.chango.controlescompuestos;
+
+/**
+ * Created by Chango on 22/11/2016.
+ */
+
+public interface OnLoginListener {
+    void onLogin(String usuario, String password);
+}
